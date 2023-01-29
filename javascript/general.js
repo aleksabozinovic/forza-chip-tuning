879966bf6cb02616542c37915a1dab41b42c0753
aleksabozinovic <aleksabozinovic722@gmail.com>
2023-01-29 02:33:26 +0100

    commit

--- javascript/general.js
+++ javascript/general.js
@@ -1,10 +1,28 @@
 "use strict";
 
+///////////////////
+// STICKY NAV
+
 let navbar = document.querySelector("nav");
 let navPos = navbar.getBoundingClientRect().top;
+let hamburger = document.querySelector("#hamburger");
+let overlay = document.querySelector(".overlay");
 
 window.addEventListener("scroll", (e) => {
   let scrollPos = window.scrollY;
   if (scrollPos > navPos) navbar.classList.add("active");
   else navbar.classList.remove("active");
+});
+
+///////////////////
+// Responsive Nav
+
+hamburger.addEventListener("click", (el) => {
+  let burger = el.target.closest("div");
+  burger.classList.toggle("open");
+  document.querySelector(".nav--col--2").classList.toggle("active");
+  if (!navbar.classList.contains("active")) navbar.classList.toggle("active");
+  document.querySelector("body").classList.toggle("stop-scroll");
+  console.log("aki");
+  overlay.classList.toggle("active");
 });
