cf64af121b18927a7f8d9c5f6d4b5fe680c74588
aleksabozinovic <aleksabozinovic722@gmail.com>
2023-02-04 01:42:42 +0100

    commit

--- javascript/slider.js
+++ javascript/slider.js
@@ -7,8 +7,6 @@ const swiper1 = new Swiper(".index--hero--marks--col-2 .swiper", {
   // Optional parameters
   loop: true,
   // Default parameters
-  slidesPerView: 1,
-  spaceBetween: 10,
   autoplay: {
     delay: 3500,
   },
@@ -16,22 +14,26 @@ const swiper1 = new Swiper(".index--hero--marks--col-2 .swiper", {
   breakpoints: {
     // when window width is >= 320px
     320: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+    425: {
       slidesPerView: 3,
-      spaceBetween: 70,
+      spaceBetween: 30,
     },
     768: {
-      slidesPerView: 6,
-      spaceBetween: 20,
+      slidesPerView: 4,
+      spaceBetween: 60,
     },
     // when window width is >= 480px
     1024: {
       slidesPerView: 5,
-      spaceBetween: 90,
+      spaceBetween: 60,
     },
     // when window width is >= 640px
     1440: {
       slidesPerView: 6,
-      spaceBetween: 90,
+      spaceBetween: 85,
     },
   },
 });
